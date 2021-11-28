7b4d51585f7128b6141e6c21e997bf02e133080f
soumyabhardwaj <63638319+soumyabhardwaj@users.noreply.github.com>
2021-11-29 00:23:40 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-calendar/dist/Calendar.css'
+import './app/layout/styles.css'
+import App from './app/layout/App'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import configureStore, { history } from './app/store/configureStore'
+import ScrollToTop from './app/layout/ScrollToTop'
+import { ConnectedRouter } from 'connected-react-router'
+
+const store = configureStore()
+
+const rootElement = document.getElementById('root')
+
+function render() {
+    ReactDOM.render(
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <ScrollToTop />
+                <App />
+            </ConnectedRouter>
+        </Provider>, 
+        rootElement
+    )
+}
+
+if (module.hot) {
+    module.hot.accept('./app/layout/App', function() {
+        setTimeout(render)
+    })
+}
+
+render()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
